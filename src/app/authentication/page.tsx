9f85a7f67c24c61ff7d94aeb1aaa793b8ddb8bab
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2025-06-05 17:51:36 -0300

    feat: add sign up form

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,29 +1,9 @@
 import React from "react";
-import { z } from "zod";
 
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-const registerFormSchema = z.object({
-  name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
-  email: z
-    .string()
-    .trim()
-    .min(1, { message: "Email é obrigatório" })
-    .email({ message: "Email inválido" }),
-  password: z
-    .string()
-    .trim()
-    .min(8, { message: "Senha deve ter pelo menos 8 caracteres" }),
-});
+import LoginForm from "./components/login-form";
+import SignUpForm from "./components/sign-up-form";
 
 const AuthenticationPage = () => {
   return (
@@ -35,28 +15,10 @@ const AuthenticationPage = () => {
             <TabsTrigger value="register">Criar conta</TabsTrigger>
           </TabsList>
           <TabsContent value="login">
-            <Card>
-              <CardHeader>
-                <CardTitle>Login</CardTitle>
-                <CardDescription>Faça login para continuar</CardDescription>
-              </CardHeader>
-              <CardContent className="grid gap-6"></CardContent>
-              <CardFooter>
-                <Button>Entrar</Button>
-              </CardFooter>
-            </Card>
+            <LoginForm />
           </TabsContent>
           <TabsContent value="register">
-            <Card>
-              <CardHeader>
-                <CardTitle>Register</CardTitle>
-                <CardDescription>Crie uma conta para continuar</CardDescription>
-              </CardHeader>
-              <CardContent className="grid gap-6"></CardContent>
-              <CardFooter>
-                <Button>Criar conta</Button>
-              </CardFooter>
-            </Card>
+            <SignUpForm />
           </TabsContent>
         </Tabs>
       </div>
