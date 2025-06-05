37780615f3faaf0c5ef522d1f619830ee88a2bea
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2025-06-04 21:02:30 -0300

    feat:  add form login

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -9,64 +10,51 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const registerFormSchema = z.object({
+  name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
+  email: z
+    .string()
+    .trim()
+    .min(1, { message: "Email é obrigatório" })
+    .email({ message: "Email inválido" }),
+  password: z
+    .string()
+    .trim()
+    .min(8, { message: "Senha deve ter pelo menos 8 caracteres" }),
+});
 
 const AuthenticationPage = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <Tabs defaultValue="account">
+        <Tabs defaultValue="login">
           <TabsList>
-            <TabsTrigger value="account">Account</TabsTrigger>
-            <TabsTrigger value="password">Password</TabsTrigger>
+            <TabsTrigger value="login">Login</TabsTrigger>
+            <TabsTrigger value="register">Criar conta</TabsTrigger>
           </TabsList>
-          <TabsContent value="account">
+          <TabsContent value="login">
             <Card>
               <CardHeader>
-                <CardTitle>Account</CardTitle>
-                <CardDescription>
-                  Make changes to your account here. Click save when you&apos;re
-                  done.
-                </CardDescription>
+                <CardTitle>Login</CardTitle>
+                <CardDescription>Faça login para continuar</CardDescription>
               </CardHeader>
-              <CardContent className="grid gap-6">
-                <div className="grid gap-3">
-                  <Label htmlFor="tabs-demo-name">Name</Label>
-                  <Input id="tabs-demo-name" defaultValue="Pedro Duarte" />
-                </div>
-                <div className="grid gap-3">
-                  <Label htmlFor="tabs-demo-username">Username</Label>
-                  <Input id="tabs-demo-username" defaultValue="@peduarte" />
-                </div>
-              </CardContent>
+              <CardContent className="grid gap-6"></CardContent>
               <CardFooter>
-                <Button>Save changes</Button>
+                <Button>Entrar</Button>
               </CardFooter>
             </Card>
           </TabsContent>
-          <TabsContent value="password">
+          <TabsContent value="register">
             <Card>
               <CardHeader>
-                <CardTitle>Password</CardTitle>
-                <CardDescription>
-                  Change your password here. After saving, you&apos;ll be logged
-                  out.
-                </CardDescription>
+                <CardTitle>Register</CardTitle>
+                <CardDescription>Crie uma conta para continuar</CardDescription>
               </CardHeader>
-              <CardContent className="grid gap-6">
-                <div className="grid gap-3">
-                  <Label htmlFor="tabs-demo-current">Current password</Label>
-                  <Input id="tabs-demo-current" type="password" />
-                </div>
-                <div className="grid gap-3">
-                  <Label htmlFor="tabs-demo-new">New password</Label>
-                  <Input id="tabs-demo-new" type="password" />
-                </div>
-              </CardContent>
+              <CardContent className="grid gap-6"></CardContent>
               <CardFooter>
-                <Button>Save password</Button>
+                <Button>Criar conta</Button>
               </CardFooter>
             </Card>
           </TabsContent>
